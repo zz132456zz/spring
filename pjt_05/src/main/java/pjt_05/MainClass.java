fb0ade1c48cee41da613a746603335f4e4c62c9d
zz132456zz <zz132456zz@kakao.com>
2020-12-30 20:24:11 +0900

    05:another way to create project

---
+++ pjt_05/src/main/java/pjt_05/MainClass.java
@@ -0,0 +1,18 @@
+package pjt_05;
+
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class MainClass {
+	
+	public static void main(String[] args) {
+		
+		GenericXmlApplicationContext ctx = new GenericXmlApplicationContext("classpath:applicationContext.xml");
+		
+		TransportationWalk transportationWalk = ctx.getBean("tWalk", TransportationWalk.class);
+		transportationWalk.move();
+		
+		ctx.close();
+		
+	}
+
+}
---
+++ pjt_05/src/main/java/pjt_05/TransportationWalk.java
@@ -0,0 +1,9 @@
+package pjt_05;
+
+public class TransportationWalk {
+
+	public void move() {
+		System.out.println("도보로 이동합니다.");
+	}
+	
+}
