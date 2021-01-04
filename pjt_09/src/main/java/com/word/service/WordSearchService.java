011f5febf20126a59d8ce803875d6dab3afca81d
zz132456zz <zz132456zz@kakao.com>
2021-01-04 16:07:42 +0900

    09:@Autowired

---
+++ pjt_09/src/main/java/com/word/service/WordSearchService.java
@@ -0,0 +1,36 @@
+package com.word.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.word.WordSet;
+import com.word.dao.WordDao;
+
+public class WordSearchService {
+	
+	private WordDao wordDao;
+	
+	@Autowired
+	public WordSearchService(WordDao wordDao) {
+		this.wordDao = wordDao;
+	}
+	
+	public WordSet searchWord(String wordKey) {
+		if(verify(wordKey)) {
+			return wordDao.select(wordKey);
+		} else {
+			System.out.println("WordKey information is available.");
+		}
+		
+		return null;
+	}
+	
+	public boolean verify(String wordKey){
+		WordSet wordSet = wordDao.select(wordKey);
+		return wordSet != null ? true : false;
+	}
+	
+	public void setWordDao(WordDao wordDao) {
+		this.wordDao = wordDao;
+	}
+	
+}
